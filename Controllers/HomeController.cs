271d80cba4bfd0f434befc2c10c342d34df1d5eb
601719180 <601719180@qq.com>
2020-05-23 18:06:05 +0800

    添加项目文件。

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JWBlog.Controllers
+{
+    public class HomeController : Controller
+    {
+        //首页
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //注册
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        //登录
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+    }
+}
